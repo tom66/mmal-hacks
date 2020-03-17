999b3cf989d02eb90fc3379ef4c774c9b86066af
Tom Oldbury! <toldbury@gmail.com>
2020-03-17 18:17:49 +0000

    Attempt to fix mmal header bug

--- raspiraw.c
+++ raspiraw.c
@@ -226,7 +226,8 @@ static void callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer)
 		file = fopen(filename, "wb");
 		if(file) {
 			printf("Writing file...\n");
-			fwrite(buffer->data, buffer->length, 1, file);
+			// skip first 32 bytes.  Packets are transmitted with 32 NUL bytes first to avoid MMAL header bug(?)
+			fwrite(buffer->data + 32, buffer->length - 32, 1, file);
 			printf("Closing file...\n");
 			fclose(file);
 			printf("Done\n");
