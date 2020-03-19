37f58ce9ce33b177f71fbba23914377bce589f8d
Tom Oldbury! <toldbury@gmail.com>
2020-03-19 18:44:58 +0000

    try to reposition waveform

--- raspiraw.c
+++ raspiraw.c
@@ -351,6 +351,7 @@ int init_resources()
 		printf("texture %d has id %d\n", i, texture_id[i]);
 		glBindTexture(GL_TEXTURE_2D, texture_id[i]);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, TEXSIZE, NWAVES, 0, GL_LUMINANCE, GL_BYTE, graph[i]);
+		
 		/* Set texture wrapping mode */
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, clamp ? GL_CLAMP_TO_EDGE : GL_REPEAT);
 		
@@ -417,10 +418,10 @@ void graph_set_buffer(MMAL_BUFFER_HEADER_T *buf)
 	int i=0;
 
 	for (int i=0; i<NTEXTURES; i++) {
-		void *ptr = (uint8_t *)((buf->data + (262144 / 2) - TEXSIZE) + (i * TEXSIZE)); // + i * TEXSIZE * NWAVES;
+		void *ptr = (int8_t *)((buf->data + (262144 / 2) - TEXSIZE) + (i * TEXSIZE)); // + i * TEXSIZE * NWAVES;
 		
 		glBindTexture(GL_TEXTURE_2D, texture_id[i]);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, TEXSIZE, 1, 0, GL_LUMINANCE, GL_SHORT, ptr);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, TEXSIZE, 1, 0, GL_LUMINANCE, GL_BYTE, ptr);
 
 		/* Set texture wrapping mode */
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, clamp ? GL_CLAMP_TO_EDGE : GL_REPEAT);
